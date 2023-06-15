a281995c47eb999326efc7f47895ca3ba55d397f
i3elj <guisande.bernardo@gmail.com>
2023-06-15 18:51:48 -0300

    style guide added for easy styling

---
+++ src/style-guide.js
@@ -0,0 +1,13 @@
+const Theme = {
+    light: {
+       green0: "#142700",
+       green1: "#90A17E",
+
+       gray0: "#D9D9D9",
+       gray1: "#666666",
+
+       blue1: "#3C6E93",
+    },
+}
+
+export default Theme;
