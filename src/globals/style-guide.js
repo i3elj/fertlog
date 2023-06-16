d10d53d59b528a1de07d931e48e5143780b2b668
i3elj <guisande.bernardo@gmail.com>
2023-06-16 17:36:25 -0300

    style guide moved to globals/ folder

---
+++ src/globals/style-guide.js
@@ -0,0 +1,14 @@
+const Theme = {
+    light: {
+       green0: '#142700',
+       green1: '#90A17E',
+
+       gray0: '#666666',
+       gray1: '#D9D9D9',
+       gray2: '#F5F5F5',
+
+       blue1: '#3C6E93',
+    },
+}
+
+export default Theme;
