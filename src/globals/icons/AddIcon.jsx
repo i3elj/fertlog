39eab45253c47bc0a78fd496767f7fdaf022ab4f
i3elj <guisande.bernardo@gmail.com>
2023-06-17 15:05:07 -0300

    new icon

---
+++ src/globals/icons/AddIcon.jsx
@@ -0,0 +1,10 @@
+import { Svg, Path } from 'react-native-svg'
+
+export default function AddIcon() {
+    return <Svg width="38" height="38" viewBox="0 0 38 38" fill="none">
+        <Path d="M19 36L19 2" stroke="#777777"
+        strokeWidth="4" strokeLinecap="round"/>
+        <Path d="M2 19L36 19" stroke="#777777"
+        strokeWidth="4" strokeLinecap="round"/>
+    </Svg>
+}
