0df34f91b19c43d29af13b2b21d170ab12003ea3
i3elj <guisande.bernardo@gmail.com>
2023-06-19 22:25:37 -0300

    line is now rendered through the new component and <StatusBar/> was moved to the <Header/> component

--- src/views/searchAreas/index.jsx
+++ src/views/searchAreas/index.jsx
@@ -7,19 +7,15 @@ import {
  } from 'react-native'
 
 import Theme from '../../globals/style-guide'
+import Line from '../../globals/line'
 import Header from '../../globals/header'
 
 import Lista from './lista'
 
 export default function SearchAreas() {
     return <ScrollView>
-        <StatusBar barStyle="dark-content" backgroundColor="#fff" />
         <Header />
-        <View style={{
-            marginHorizontal: 20,
-            borderBottomWidth: 3,
-            borderColor: Theme.light.gray2
-        }} />
+        <Line />
         <Lista />
     </ScrollView>
 }
