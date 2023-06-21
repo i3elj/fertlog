a45c5eae0eccdcba68f3750244eb6d624cf8b5ce
i3elj <guisande.bernardo@gmail.com>
2023-06-21 16:56:36 -0300

    new icons

---
+++ src/globals/icons/homeIcon.jsx
@@ -0,0 +1,7 @@
+import { Svg, Path } from 'react-native-svg'
+
+export default function HomeIcon() {
+    return <Svg width="46" height="46" viewBox="0 0 46 46" fill="none">
+        <Path d="M7.66666 40.25V17.25L23 5.75L38.3333 17.25V40.25H26.8333V26.8333H19.1667V40.25H7.66666Z" fill="black"/>
+    </Svg>
+}
