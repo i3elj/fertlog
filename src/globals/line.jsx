17a21a21806c10b6f6cb0b050e6523e0d62bff74
i3elj <guisande.bernardo@gmail.com>
2023-06-22 14:25:08 -0300

    added style prop

--- src/globals/line.jsx
+++ src/globals/line.jsx
@@ -2,12 +2,12 @@ import { View } from 'react-native'
 
 import Theme from './style-guide'
 
-const Line = () => (
-    <View style={{
+const Line = ({ style }) => (
+    <View style={[{
         marginHorizontal: 20,
         borderBottomWidth: 3,
         borderColor: Theme.light.gray2
-    }} />
+    }, style]} />
 )
 
 export default Line;
